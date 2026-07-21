838eca2d0c48cc5a087858ea5e1447facdd09caf
reizo00 <reizou000@gmail.com>
2026-07-21 13:37:45 +0900

    clientと統合！

--- src/client/java/com/chat_heads_figura/client/ChatHeadsFiguraSupportClient.java
+++
@@ -1,9 +0,0 @@
-package com.chat_heads_figura.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class ChatHeadsFiguraSupportClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-	}
-}
