fc1ae0ffcd5ba66446dd239c63d9671942fd5a26
reizo00 <reizou000@gmail.com>
2026-07-21 17:09:49 +0900

    ライセンスをちょっと強めてみた、アドバイスありがとう！

--- src/main/java/com/chat_heads_figura/interfaces/ChatHeadsAvatar.java
+++ src/main/java/com/chat_heads_figura/interfaces/ChatHeadsAvatar.java
@@ -3,6 +3,14 @@ package com.chat_heads_figura.interfaces;
 import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Unique;
 
+/*
+ * Copyright (c) 2026 reizou
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 public interface ChatHeadsAvatar {
     @Unique
     boolean chatHeads$renderPortrait(GuiGraphics gui, int x, int y, int size, float modelScale, boolean upsideDown, float alpha);
