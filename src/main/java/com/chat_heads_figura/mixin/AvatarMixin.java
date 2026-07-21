a5c3011180034cfc67714336a48968b83c81306b
reizo00 <reizou000@gmail.com>
2026-07-21 16:50:40 +0900

    ライセンスをちょっと強めてみた、アドバイスありがとう！

--- src/main/java/com/chat_heads_figura/mixin/AvatarMixin.java
+++ src/main/java/com/chat_heads_figura/mixin/AvatarMixin.java
@@ -18,6 +18,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
+/*
+ * Copyright (c) 2026 reizou
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 @Mixin(Avatar.class)
 public abstract class AvatarMixin implements ChatHeadsAvatar {
 
--- src/main/java/com/chat_heads_figura/mixin/ChatHeadsMixin.java
+++ src/main/java/com/chat_heads_figura/mixin/ChatHeadsMixin.java
@@ -14,6 +14,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+/*
+ * Copyright (c) 2026 reizou
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 @Mixin(ChatHeads.class)
 public class ChatHeadsMixin {
     @Inject(
